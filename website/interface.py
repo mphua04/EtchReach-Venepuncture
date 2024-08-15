f83bd556b6b3dce005443ab5662c3c85a2d8a435
blueetfm <tan.yx2004@gmail.com>
2024-08-15 19:16:23 +0800

    fix: bug fix

--- website/interface.py
+++ website/interface.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, render_template, redirect, request, session
 
 app = Flask(__name__)
 
-@app.route('/')
+# Constants
+constAspects = ['Angle Orientation', 'Tourniquet time', 'Flashback']
+aspects={
+    'Angle Orientation': {
+        'measurement' : None,
+        'result' : None
+    },
+    'Tourniquet Time': {
+        'measurement' : None,
+        'result': None
+    },
+    'Flashback': {
+        'measurement' : None,
+        'result': None
+    }
+}
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('interface.html')
+    if request.method == "GET":
+        return render_template('interface.html')
+
+
+
+
+@app.route('/prepPhase', methods=["GET", "POST"])
+def prepChecklist():
+    if request.method == "GET":
+        return render_template('prepPhase.html')
+
+
+@app.route('/results', methods=["GET"])
+def feedback():
+
+    
+    return render_template('results.html', aspects=aspects)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
